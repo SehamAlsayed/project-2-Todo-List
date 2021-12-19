be4a5383efda9cba7e5033a5094d2748a08c459d
SehamAlsayed <s.alsayed141@gmail.com>
2021-12-19 18:46:28 +0300

    finish get and post

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,34 @@ const db = require('./db')
 const Todo = require('./todo')
 //console.log(Todo);
 
+app.use(express.json());
+
 app.get('/', (req,res) => {
     res.json('GET / is working')
 })
 
 app.get('/tasks', (req,res) => {
-    res.json('GET / is working')
-})
+    Todo.find({}, (err, data) => {
+        if (err) {
+            console.log("ERROR: ", err);
+        }else {
+            res.json(data);
+        }
+    });   
+});
+
+app.post('/tasks', (req,res) => {
+console.log('25:', req.body)
+    Todo.create(req.body, (err, newTask) => {
+       if (err) {
+            console.log("ERROR: ", err);
+        }else {
+            res.status(201).json(newTask);
+        }
+    });   
+});
+
+
 
 app.listen(5000, () => {
     console.log('SERVER IS WORKING');
