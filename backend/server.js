36ec964afb7948fd5b878069bcae2774b4feb999
SehamAlsayed <s.alsayed141@gmail.com>
2021-12-20 11:10:18 +0300

    finish delete and put

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
-const express= require('express')
-const app = express()
-const db = require('./db')
-const Todo = require('./todo')
+const express= require('express');
+const app = express();
+
+const db = require('./db');
+const Todo = require('./todo');
 //console.log(Todo);
 
 app.use(express.json());
 
 app.get('/', (req,res) => {
-    res.json('GET / is working')
-})
+    res.json('GET / is working');
+});
 
 app.get('/tasks', (req,res) => {
     Todo.find({}, (err, data) => {
@@ -21,7 +22,8 @@ app.get('/tasks', (req,res) => {
 });
 
 app.post('/tasks', (req,res) => {
-console.log('25:', req.body)
+  //console.log('25:', req.body)
+
     Todo.create(req.body, (err, newTask) => {
        if (err) {
             console.log("ERROR: ", err);
@@ -31,7 +33,38 @@ console.log('25:', req.body)
     });   
 });
 
+app.delete('/tasks/:id', (req,res) => {
+   // console.log('37:', req.params.id)
 
+     Todo.deleteOne({ _id: req.params.id }, (err, deleteObj) => {
+         if (err) {
+                console.log("ERROR: ", err);
+         } else {
+                deleteObj.deletedCount === 1
+              ? res.json("Delete one todo successfully")
+              : res.status(404).json("This todo is not found");
+            }
+        });   
+    });
+
+app.put('/tasks/:id', (req,res) => {
+      ///  console.log('37:', req.params.id)
+
+        Todo.updateOne(
+             { _id: req.params.id },
+             { title: req.body.newTitle},
+             (err, updateObj) => {
+               if (err) {
+                 console.log("ERROR: ", err);
+                    res.status(400).json(err)
+                } else {
+                    console.log(updateObj);
+                    updateObj.modifiedCount === 1
+                  ? res.json("Update one todo successfully")
+                  : res.status(404).json("This todo is not found")
+                }
+            });   
+        });
 
 app.listen(5000, () => {
     console.log('SERVER IS WORKING');
