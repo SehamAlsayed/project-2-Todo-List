3428fb84a5d998a10ea3e106789bd6978dd7b6e1
SehamAlsayed <s.alsayed141@gmail.com>
2021-12-25 19:28:52 +0300

    finish create new task and show isCompleted

---
+++ frontend/src/App.js
@@ -0,0 +1,64 @@
+import React, {useEffect, useState} from 'react'
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import Todo from './components/Todo';
+import Add from './components/Add';
+
+
+
+export default function App() {
+
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    getData()
+  },[]);
+
+  const getData=() => {
+    axios
+    .get('http://localhost:5000/tasks')
+    .then((response) => {
+      console.log('RESPONSE: ', response);
+      console.log('DATA: ', response.data);
+      setTasks(response.data)
+    })
+.catch((err) => {
+  console.log('ERR: ', err);
+});
+  }
+
+  const postNewTodo =(body) => {
+   // console.log("App func postNewTodo from App");
+    axios
+    .post(`http://localhost:5000/tasks`,body)
+    .then((response) => {
+     // console.log('RESPONSE: ', response);
+      console.log('DATA: ', response.data);
+     // setTasks(response.data) 
+     getData()
+     //change state using spread operator >>> search about this
+    })
+.catch((err) => {
+  console.log('ERR: ', err); 
+});
+  }
+
+
+  const mapOverTasks=tasks.map((taskObj,i) => ( 
+  <Todo key={i} task={taskObj} />
+  ));
+
+  return (
+    <div className='App'>
+  <p>app</p>
+  <Add createFunc={postNewTodo}/>
+  <button onClick={getData}>GET TASKS</button>
+
+  {mapOverTasks}
+  
+    </div>
+  )  
+}
+
+
---
+++ frontend/src/components/Add.js
@@ -0,0 +1,25 @@
+//import { PromiseProvider } from "mongoose";
+import React ,{useState} from "react";
+
+export default function Add(Props) {
+const [newTitle, setNewTitle] = useState('')
+
+
+    const createNewTodo=()=>{
+console.log('createNewTodo from ADD');
+//{"title":"task 5", "isCompleted": false}
+Props.createFunc({title: newTitle, isCompleted:false});
+    };
+
+    return (
+        <div className="Add">
+<input type="text" placeholder="Write new title here...."  
+onChange={(e)=>{
+    setNewTitle(e.target.value)
+}} />
+<button onClick={createNewTodo}>Create New Todo</button>
+
+
+        </div>
+    )
+}
